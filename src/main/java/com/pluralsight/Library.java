0f6477782bb148ef7d36485769a9cad6541bdd82
praytoo <praytoo52@icloud.com>
2025-10-03 15:42:00 -0700

    initial commit

---
+++ src/main/java/com/pluralsight/Library.java
@@ -0,0 +1,77 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Library {
+    static Book[] books = loadBooks();
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pick 1 if you want to see all available books Pick 2 if you want to check out a book");
+        int choice = scanner.nextInt();
+        //while (true){
+        switch (choice) {
+            case 1:
+                showAllBooks();
+                break;
+            case 2:
+                checkOutBook(books[1]);
+                break;
+        }
+        //}
+    }
+
+    public static void showAllBooks() {
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getTitle());
+        }
+    }
+
+    public static void checkOutBook(Book book) {
+        book.checkOut("Prince");
+    }
+
+    public static Book[] loadBooks() {
+        return new Book[]{
+                new Book(1, "9780140449136", "The Odyssey", false, ""),
+                new Book(2, "9780439139601", "Harry Potter and the Goblet of Fire", true, "User123"),
+                new Book(3, "9780141439600", "Pride and Prejudice", false, ""),
+
+                new Book(4, "9780307476463", "The Road", true, "User456"),
+
+                new Book(5, "9780743273565", "The Great Gatsby", false, ""),
+
+                new Book(6, "9780553386790", "A Game of Thrones", true, "User789"),
+
+                new Book(7, "9780061120084", "To Kill a Mockingbird", false, ""),
+
+                new Book(8, "9780385472579", "Zen and the Art of Motorcycle Maintenance", false, ""),
+
+                new Book(9, "9780345339706", "The Hobbit", true, "User234"),
+
+                new Book(10, "9781451673319", "Fahrenheit 451", false, ""),
+
+                new Book(11, "9780142437230", "Moby-Dick", false, ""),
+
+                new Book(12, "9780060850524", "Brave New World", true, "User876"),
+
+                new Book(13, "9780143105985", "Meditations", false, ""),
+
+                new Book(14, "9780525566151", "Where the Crawdads Sing", false, ""),
+
+                new Book(15, "9780679783268", "1984", true, "User345"),
+
+                new Book(16, "9780747532743", "Harry Potter and the Philosopher's Stone", false, ""),
+
+                new Book(17, "9780812981605", "The Power of Habit", false, ""),
+
+                new Book(18, "9780307277671", "The Kite Runner", true, "User567"),
+
+                new Book(19, "9780316769488", "The Catcher in the Rye", false, ""),
+
+                new Book(20, "9780062315007", "Sapiens: A Brief History of Humankind", true, "User999")
+
+        };
+    }
+}
